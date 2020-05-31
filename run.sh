47c562537c305edefb6df58bd2285726d320bce9
Germain GAU <germain.gau@gmail.com>
2020-05-31 21:36:55 +0200

    wav encoding

--- run.sh
+++ run.sh
@@ -1,9 +1,33 @@
-sudo rm -rf /tmp/recordings
+
+sudo rm -rf /tmp/recordings || true
 docker build -t recorder .
 docker run \
     -p 5900:5900 \
     -v /tmp/recordings:/tmp/recordings \
     -it \
-    recorder
-cat $(ls -tr /tmp/recordings/** | xargs) > /tmp/record.webm
-vlc /tmp/record.webm
+    recorder 
+
+find /tmp/recordings -type f \
+    | xargs stat -c '%Y %n' \
+    | sort \
+    | awk '{print $2}' \
+    | xargs cat \
+    > /tmp/record.webm \
+    && \
+    sudo rm -rf /tmp/recordings
+yes | ffmpeg \
+    -hide_banner -loglevel panic \
+    -i /tmp/record.webm \
+    -acodec copy \
+    /tmp/out_stereo.wav \
+    && \
+    rm /tmp/record.webm
+yes | ffmpeg \
+    -hide_banner -loglevel panic \
+    -i /tmp/out_stereo.wav \
+    -ac 1 \
+    /tmp/out_mono.wav \
+    && \
+    rm /tmp/out_stereo.wav
+
+vlc /tmp/out_mono.wav
